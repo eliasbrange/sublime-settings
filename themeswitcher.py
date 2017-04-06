2f205d1cd050c1307d966a1d8b1254015b114f21
Elias Brange <elias.brange@lundalogik.se>
2017-04-06 15:46:07 +0200

    Change accent colors

--- themeswitcher.py
+++ themeswitcher.py
@@ -6,10 +6,14 @@ class ThemeDarkCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         path = 'Preferences.sublime-settings'
         settings = sublime_plugin.sublime.load_settings(path)
-        settings.set("color_scheme",
-                     "Packages/User/SublimeLinter/Tomorrow-Night (SL).tmTheme")
-        settings.set("theme",
-                     "Boxy Tomorrow.sublime-theme")
+        settings.set('color_scheme',
+                     'Packages/User/SublimeLinter/Tomorrow-Night (SL).tmTheme')
+        settings.set('theme',
+                     'Boxy Tomorrow.sublime-theme')
+        settings.set('theme_accent_numix',
+                     True)
+        settings.set('theme_accent_tangerine',
+                     False)
         settings = sublime_plugin.sublime.save_settings(path)
 
 
@@ -18,9 +22,13 @@ class ThemeLightCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         path = 'Preferences.sublime-settings'
         settings = sublime_plugin.sublime.load_settings(path)
-        settings.set("color_scheme",
-                     "Packages/User/SublimeLinter/Tomorrow (SL).tmTheme")
-        settings.set("theme",
-                     "Boxy Solarized Light.sublime-theme")
+        settings.set('color_scheme',
+                     'Packages/User/SublimeLinter/Tomorrow (SL).tmTheme')
+        settings.set('theme',
+                     'Boxy Solarized Light.sublime-theme')
+        settings.set('theme_accent_numix',
+                     False)
+        settings.set('theme_accent_tangerine',
+                     True)
         settings = sublime_plugin.sublime.save_settings(path)
 
